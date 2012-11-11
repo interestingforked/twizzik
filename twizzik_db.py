a05095408cb507ef7be7aa00475815a578092a4c
CodyHenshaw <cghensha@eagle.fgcu.edu>
2012-11-11 06:48:44 -0500

    Updating with changes

--- twizzik_db.py
+++ twizzik_db.py
@@ -1,43 +1,76 @@
 import MySQLdb as mdb
 import sys
 
+	
+def add_tracking_user(user_id,screen_name):
 
-def all_users():
-    
-    '''selects all information from `Users` table'''
+    con = mdb.connect('localhost','twizzik','twIzz1k','twizzik')   
+    cur = con.cursor()
+    cur.execute("INSERT INTO `User_Tracking_Ids` (`id`,`screen_name`,`dt_added`) VALUES (%s,%s,CURDATE())",(user_id,screen_name))
+    print "Added User to Tracking"
+
+def all_tracking_user():
 
     con = mdb.connect('localhost','twizzik','twIzz1k','twizzik')
-    
-    with con:
-        # create a new cursor object 
-        cur = con.cursor()
-	# execute the query
-        cur.execute("SELECT * FROM `Users`")
-        # get all rows
-        rows = cur.fetchall()
-	# get headers from cursor object
-        desc = cur.description
-	# print out the headers (for now)
-	col_name_list = [tuple[0] for tuple in cur.description]	
-	# print out our rows of data		 
-	for row in rows:
-            print row
+    cur = con.cursor()	
+    cur.execute("SELECT * FROM `User_Tracking_Ids`")
+    rows = cur.fetchall()
+    for row in rows:
+        print row
 
-def add_whitelist_user(user_id,screen_name):
+def all_user():
     
-    '''adds a user to the whitelist'''
-     
-    con = mdb.connect('localhost','twizzik','twIzz1k','twizzik')
+    '''selects all information from `User` table'''
+
+    con = mdb.connect('localhost','twizzik','twIzz1k','twizzik')   
+    cur = con.cursor()
+    cur.execute("SELECT * FROM `User`")
+    rows = cur.fetchall()
+    desc = cur.description      
+    print "\n%15s %15s %15s %15s" % (desc[0][0],desc[1][0],desc[2][0],desc[3][0])
+    for row in rows:
+        print row
+
+def add_whitelist_user(user_id,screen_name,my_name):
     
-    with con:
-	# create a new cursor object
+    '''adds a user to the whitelist'''    
+    
+    con = mdb.connect('localhost','twizzik','twIzz1k','twizzik')   
+    cur = con.cursor()
+    cur.execute("INSERT INTO `White_List` (`id`,`screen_name`,`dt_whitelisted`,`whitelisted_by`) VALUES (%s,%s,CURDATE(),%s)",(user_id,screen_name,my_name))
+    print "Added User to White List for " + my_name + "\n"
+
+def all_whitelist_user():
+
+	con = mdb.connect('localhost','twizzik','twIzz1k','twizzik')
 	cur = con.cursor()
-	# execute the query
-        try:
-            cur.execute("""INSERT INTO `White_List` (`id`,`screen_name`,`dt_whitelisted`) VALUES (%s,%s,CURDATE())""",(user_id,screen_name))
-	    con.commit()
-	    print "Added User to White List"
-	except:
-	    con.rollback()
-	    print "Failed to Add User to White List"
+	cur.execute("SELECT * FROM `White_List`")
+	rows = cur.fetchall()
+	desc = cur.description
+	print "\n%15s %15s %15s %15s" % (desc[0][0],desc[1][0],desc[2][0],desc[3][0])
+	print "-" * 65
+	for row in rows:
+		print "%15s %15s %15s %15s" % row
+        print "-" * 65 + "\n"
 
+def check_whitelist(non_reciprocal):
+    
+    '''checks whitelist against non_reciprocal followers list'''
+    con = mdb.connect('localhost','twizzik','twIzz1k','twizzik')
+    cur = con.cursor()
+    cur.execute("SELECT `id` FROM `White_List`")
+    whitelist_users = []
+    rows = cur.fetchall()
+    for row in rows:
+        whitelist_users.append(row)
+    for users in whitelist_users:
+        print users
+
+ 
+   
+
+
+
+
+
+    
